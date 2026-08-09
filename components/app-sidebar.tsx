98836f904440fd5379011f7002acc0528971cb8a
Aditya Ashok Mane <maneaditya006@gmail.com>
2026-08-09 16:50:59 +0530

    Implemented the Mapping of sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,194 +1,228 @@
-"use client"
+'use client';
 
-import * as React from "react"
+import * as React from 'react';
 
-import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavUser } from "@/components/nav-user"
-import { TeamSwitcher } from "@/components/team-switcher"
+import { NavMain } from '@/components/nav-main';
+import { NavProjects } from '@/components/nav-projects';
+import { NavUser } from '@/components/nav-user';
+import { TeamSwitcher } from '@/components/team-switcher';
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
+  SidebarGroup,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
   SidebarRail,
-} from "@/components/ui/sidebar"
-import { GalleryVerticalEndIcon, AudioLinesIcon, TerminalIcon, TerminalSquareIcon, BotIcon, BookOpenIcon, Settings2Icon, FrameIcon, PieChartIcon, MapIcon } from "lucide-react"
+} from '@/components/ui/sidebar';
+import {
+  GalleryVerticalEndIcon,
+  AudioLinesIcon,
+  TerminalIcon,
+  TerminalSquareIcon,
+  BotIcon,
+  BookOpenIcon,
+  Settings2Icon,
+  FrameIcon,
+  PieChartIcon,
+  MapIcon,
+  Users,
+  UsersRound,
+  KeyRound,
+  ShieldUser,
+  FileKey,
+  Building2,
+  Mail,
+  History,
+  ChevronRightIcon,
+} from 'lucide-react';
+import useGetPermissionQuery from '@/hooks/useGetPermissionQuery';
+import { Skeleton } from '@/components/ui/skeleton';
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from './ui/collapsible';
 
-// This is sample data.
-const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
-  teams: [
-    {
-      name: "Acme Inc",
-      logo: (
-        <GalleryVerticalEndIcon
-        />
-      ),
-      plan: "Enterprise",
-    },
-    {
-      name: "Acme Corp.",
-      logo: (
-        <AudioLinesIcon
-        />
-      ),
-      plan: "Startup",
-    },
-    {
-      name: "Evil Corp.",
-      logo: (
-        <TerminalIcon
-        />
-      ),
-      plan: "Free",
-    },
-  ],
-  navMain: [
-    {
-      title: "Playground",
-      url: "#",
-      icon: (
-        <TerminalSquareIcon
-        />
-      ),
-      isActive: true,
-      items: [
-        {
-          title: "History",
-          url: "#",
-        },
-        {
-          title: "Starred",
-          url: "#",
-        },
-        {
-          title: "Settings",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: (
-        <BotIcon
-        />
-      ),
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
-        },
-        {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: (
-        <BookOpenIcon
-        />
-      ),
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: (
-        <Settings2Icon
-        />
-      ),
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
-        },
-      ],
-    },
-  ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: (
-        <FrameIcon
-        />
-      ),
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: (
-        <PieChartIcon
-        />
-      ),
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: (
-        <MapIcon
-        />
-      ),
-    },
-  ],
+export function NavMainSkeleton() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <Skeleton className="w-full h-12" />
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <Skeleton className="w-[95%] h-8 ml-auto" />
+          <SidebarMenu className="mt-2">
+            <>
+              <Skeleton className="w-[95%] ml-auto h-8" />
+
+              <Collapsible
+                asChild
+                defaultOpen={true}
+                className="group/collapsible"
+              >
+                <SidebarMenuItem>
+                  <CollapsibleTrigger asChild></CollapsibleTrigger>
+                  <CollapsibleContent>
+                    <SidebarMenuSub>
+                      <Skeleton className="w-full h-9 my-1" />
+                      <Skeleton className="w-full h-9 my-1" />
+                      <Skeleton className="w-full h-9 my-1" />
+                      <Skeleton className="w-full h-9 my-1" />
+                      <Skeleton className="w-full h-9 my-1" />
+                      <Skeleton className="w-full h-9 my-1" />
+                    </SidebarMenuSub>
+                  </CollapsibleContent>
+                </SidebarMenuItem>
+              </Collapsible>
+            </>
+          </SidebarMenu>
+        </SidebarGroup>
+        <SidebarGroup>
+          <Skeleton className=" h-8 w-[95%] ml-auto" />
+          <SidebarMenu className="mt-2">
+            <>
+              <Skeleton className="w-[95%] ml-auto h-8 mt-[0.35rem]" />
+              <Skeleton className="w-[95%] ml-auto h-8 mt-[0.35rem]" />
+              <Skeleton className="w-[95%] ml-auto h-8 mt-[0.35rem]" />
+            </>
+          </SidebarMenu>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <Skeleton className="w-[95%] ml-auto h-12" />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  );
 }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { permissions, isPending } = useGetPermissionQuery();
+
+  const [permissionsData, setPermissionsData] = React.useState<
+    Record<string, unknown>
+  >({});
+
+  const data = React.useMemo(
+    () => ({
+      user: {
+        name: 'shadcn',
+        email: 'm@example.com',
+        avatar: '/avatars/shadcn.jpg',
+      },
+      teams: [
+        {
+          name: 'Acme Inc',
+          logo: <GalleryVerticalEndIcon />,
+          plan: 'Enterprise',
+        },
+        {
+          name: 'Acme Corp.',
+          logo: <AudioLinesIcon />,
+          plan: 'Startup',
+        },
+        {
+          name: 'Evil Corp.',
+          logo: <TerminalIcon />,
+          plan: 'Free',
+        },
+      ],
+      navMain: [
+        {
+          title: 'IAM',
+          url: '/iam',
+          icon: <TerminalSquareIcon />,
+          isActive: false,
+          groupLabel: 'Identity and Access Management',
+          items: [
+            {
+              title: 'Users',
+              url: '/iam/users',
+              permission: permissionsData['user:read'],
+              icon: <Users />,
+            },
+            {
+              title: 'User Groups',
+              url: '/iam/user-groups',
+              permission: permissionsData['user-group:read'],
+              icon: <UsersRound />,
+            },
+            {
+              title: 'Roles',
+              url: '/iam/roles',
+              permission: permissionsData['role:read'],
+              icon: <KeyRound />,
+            },
+            {
+              title: 'Role Groups',
+              url: '/iam/role-groups',
+              permission: permissionsData['role-group:read'],
+              icon: <ShieldUser />,
+            },
+            {
+              title: 'Policies',
+              url: '/iam/policies',
+              permission: permissionsData['policy:read'],
+              icon: <FileKey />,
+            },
+            {
+              title: 'Organizations',
+              url: '/iam/organizations',
+              permission: permissionsData['organization:read'],
+              icon: <Building2 />,
+            },
+            {
+              title: 'Memberships',
+              url: '/iam/memberships',
+              permission: permissionsData['membership:read'],
+              icon: <Users />,
+            },
+            {
+              title: 'Invitations',
+              url: '/iam/invitations',
+              permission: permissionsData['invitation:read'],
+              icon: <Mail />,
+            },
+            {
+              title: 'Sessions',
+              url: '/iam/sessions',
+              permission: permissionsData['session:read'],
+              icon: <History />,
+            },
+          ],
+        },
+      ],
+    }),
+    [permissionsData],
+  );
+
+  React.useEffect(() => {
+    setPermissionsData({ ...permissions?.policy_document?.permissions });
+  }, [permissions]);
+
+  if (isPending) {
+    return <NavMainSkeleton />;
+  }
   return (
     <Sidebar collapsible="icon" {...props}>
       <SidebarHeader>
         <TeamSwitcher teams={data.teams} />
       </SidebarHeader>
       <SidebarContent>
-        <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        <NavMain items={data.navMain as any} />
       </SidebarContent>
       <SidebarFooter>
         <NavUser user={data.user} />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -1,10 +1,10 @@
-"use client"
+'use client';
 
 import {
   Collapsible,
   CollapsibleContent,
   CollapsibleTrigger,
-} from "@/components/ui/collapsible"
+} from '@/components/ui/collapsible';
 import {
   SidebarGroup,
   SidebarGroupLabel,
@@ -14,59 +14,75 @@ import {
   SidebarMenuSub,
   SidebarMenuSubButton,
   SidebarMenuSubItem,
-} from "@/components/ui/sidebar"
-import { ChevronRightIcon } from "lucide-react"
+} from '@/components/ui/sidebar';
+import { cn } from '@/lib/utils';
+import { ChevronRightIcon } from 'lucide-react';
+import React from 'react';
 
 export function NavMain({
   items,
 }: {
   items: {
-    title: string
-    url: string
-    icon?: React.ReactNode
-    isActive?: boolean
+    title: string;
+    url: string;
+    icon?: React.ReactNode;
+    isActive?: boolean;
+    permission?: boolean;
+    groupLabel?: string;
     items?: {
-      title: string
-      url: string
-    }[]
-  }[]
+      title: string;
+      url: string;
+      icon: React.ReactNode;
+      permission?: boolean;
+    }[];
+  }[];
 }) {
   return (
-    <SidebarGroup>
-      <SidebarGroupLabel>Platform</SidebarGroupLabel>
-      <SidebarMenu>
-        {items.map((item) => (
-          <Collapsible
-            key={item.title}
-            asChild
-            defaultOpen={item.isActive}
-            className="group/collapsible"
-          >
-            <SidebarMenuItem>
-              <CollapsibleTrigger asChild>
-                <SidebarMenuButton tooltip={item.title}>
-                  {item.icon}
-                  <span>{item.title}</span>
-                  <ChevronRightIcon className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
-                </SidebarMenuButton>
-              </CollapsibleTrigger>
-              <CollapsibleContent>
-                <SidebarMenuSub>
-                  {item.items?.map((subItem) => (
-                    <SidebarMenuSubItem key={subItem.title}>
-                      <SidebarMenuSubButton asChild>
-                        <a href={subItem.url}>
-                          <span>{subItem.title}</span>
-                        </a>
-                      </SidebarMenuSubButton>
-                    </SidebarMenuSubItem>
-                  ))}
-                </SidebarMenuSub>
-              </CollapsibleContent>
-            </SidebarMenuItem>
-          </Collapsible>
-        ))}
-      </SidebarMenu>
-    </SidebarGroup>
-  )
+    <>
+      {items.map((item) => (
+        <SidebarGroup>
+          <SidebarGroupLabel className="truncate">
+            {item?.groupLabel}
+          </SidebarGroupLabel>
+          <SidebarMenu>
+            <Collapsible
+              key={item.title}
+              asChild
+              defaultOpen={item.isActive}
+              className="group/collapsible"
+            >
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton tooltip={item.title}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                    <ChevronRightIcon className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    {item.items?.map((subItem) => {
+                      return (
+                        <SidebarMenuSubItem
+                          key={subItem.title}
+                          className={cn(subItem?.permission && 'none')}
+                        >
+                          <SidebarMenuSubButton asChild>
+                            <a href={subItem.url}>
+                              {subItem.icon}
+                              <span>{subItem.title}</span>
+                            </a>
+                          </SidebarMenuSubButton>
+                        </SidebarMenuSubItem>
+                      );
+                    })}
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </SidebarMenuItem>
+            </Collapsible>
+          </SidebarMenu>
+        </SidebarGroup>
+      ))}
+    </>
+  );
 }
