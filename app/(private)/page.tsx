d0a7653668365220751b2e5ff5e65a05387dcabf
Aditya Ashok Mane <maneaditya006@gmail.com>
2026-09-12 13:59:41 +0530

    Working on data table

--- app/(private)/page.tsx
+++ app/(private)/page.tsx
@@ -1,17 +1,36 @@
 import PermissionAllowed from '@/components/auth/PermissionAllowed';
 import PrivatePageLayout from '@/components/auth/PrivatePageLayout';
+import { DataTableFeatures } from '@/components/data-table/data-table-features';
+import { DataTable, Payment } from '@/components/data-table/DataTable';
+import { makeData } from '@/components/data-table/makeData';
 import { Button } from '@/components/ui/button';
+import { createColumnHelper } from '@tanstack/react-table';
 import React from 'react';
-import DataTable from '@/components/data-table/DataTable';
+
+const columnHelper = createColumnHelper<DataTableFeatures, Payment>();
+
+export const columns = columnHelper.columns([
+  columnHelper.accessor('firstName', {
+    header: 'First Name',
+  }),
+  columnHelper.accessor('lastName', {
+    header: 'Last Name',
+  }),
+  columnHelper.accessor('status', {
+    header: 'Status',
+  }),
+]);
 
 function page() {
+  const data = React.useMemo(() => makeData(100), []);
+  console.log('data : ', data);
   return (
     <PrivatePageLayout breadCrumbLinks={[{ title: 'Home', link: '/' }]}>
       {/* Home Page
       <PermissionAllowed permission="user:create">
         <Button>Create user</Button>
       </PermissionAllowed> */}
-      <DataTable />
+      <DataTable data={data} columns={columns} />
     </PrivatePageLayout>
   );
 }
